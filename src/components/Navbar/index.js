3af3ee3730fee3f6fc02e5e5915a6221f1a833b0
Tiago Soares <fullcodebr@gmail.com>
2021-05-12 19:08:48 -0300

    fix responsive and Feature btn navbar on mobile

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {NavLink } from 'react-router-dom'
 import {ContainerNavbar} from './styles';
+import {IoIosArrowForward} from 'react-icons/io';
 
-export default function index() {
+export default function Navbar() {
+
+  const [isOpen, setIsOpen] = useState('');
+  const handleOpenNavBar = () => {
+    if(isOpen === 'open'){
+      setIsOpen('')
+    }else{
+      setIsOpen('open')
+    }
+  }
   return (
     <>
-      <ContainerNavbar>
+      <ContainerNavbar className={isOpen}>
+      <span className='btn-hanburguer'onClick={handleOpenNavBar} >
+        <IoIosArrowForward />
+      </span>
        <ul className="nav-ul">
           <li>
             <NavLink activeClassName="active" exact to="/">Home</NavLink>
