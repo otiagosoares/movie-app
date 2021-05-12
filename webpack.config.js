6a3fcf81e2a0bc0408ad61137525aa20dad9998f
Tiago Soares <fullcodebr@gmail.com>
2021-05-12 18:36:07 -0300

    webpack optimization

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,9 @@ module.exports = {
   mode: 'production',
   optimization: {
     minimize: true,
+    runtimeChunk: {
+      name: (entrypoint) => `runtimechunk~${entrypoint.name}`,
+    },
+    emitOnErrors: true,
   }
 }
